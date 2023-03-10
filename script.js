843aaa65fc488f87adb161632b98aaeaef72e7e4
Ozan <ozancangu24@gmail.com>
2023-03-10 20:39:12 +0300

    request function handled

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
-
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
 
 let apiQuotes=[];
 
 function newQuote(){
      // pick a random quote from api array
      const quote=apiQuotes[Math.floor(Math.random()*apiQuotes.length)];
+     authorText.textContent=quote.author;
+     quoteText.textContent=quote.text;
+
      console.log(quote);
 }
 
