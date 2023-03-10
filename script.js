38a1bf6d776ec5092f5c6578f45a230b2291f3b3
Ozan <ozancangu24@gmail.com>
2023-03-10 20:48:11 +0300

    progress bar added

--- script.js
+++ script.js
@@ -7,17 +7,33 @@ const loader = document.getElementById('loader');
 
 let apiQuotes=[];
 
+
+// Loading Spinner Shown
+function loading() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+  }
+
+
+  // Remove Loading Spinner
+function complete() {
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+  }
 function newQuote(){
+      loading();
+
      // pick a random quote from api array
      const quote=apiQuotes[Math.floor(Math.random()*apiQuotes.length)];
      authorText.textContent=quote.author;
      quoteText.textContent=quote.text;
 
-     console.log(quote);
+     complete();
 }
 
 // get quotes from api
 async function getQuotes(){
+    loading();
     const apiUrl='https://type.fit/api/quotes';
 
     try {
